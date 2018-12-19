2a1c62cb36f57d0dea5287630583b3b0c11af837
smallship <user@example.com>
2018-12-19 11:53:39 +0000

    Implemented axo

--- tiler.py
+++ tiler.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 import pygame.gfxdraw
 
@@ -12,8 +13,8 @@ class Tiler:
         self.screen_height = 480
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption('Tiler')
-        self.map_size = (4, 4)
-        self.tile_size = 20
+        self.map_size = (10, 10)
+        self.tile_size = 60
 
         self.fps_clock = pygame.time.Clock()
         self.running = True
@@ -31,9 +32,13 @@ class Tiler:
 
     def render_frame(self):
         self.screen.fill(colors.BACKGROUND)
+        pygame.draw.rect(self.screen, colors.RED, ((0, 0), (self.tile_size, self.tile_size)))
+        # pygame.draw.rect(self.screen, colors.RED, ((100, 100), (self.tile_size, self.tile_size)))
+
         for x in range(self.map_size[0]):
             for y in range(self.map_size[1]):
-                self.draw_tile(*self.camera(x * self.tile_size, y * self.tile_size))
+                world_x, world_y = x * self.tile_size, y * self.tile_size
+                self.draw_tile(world_x, world_y)
 
         # limit fps
         self.fps_clock.tick(60)
@@ -53,19 +58,26 @@ class Tiler:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                     self.running = False
+                    print("quit")
 
-    def camera(self, cart_x, cart_y):
-        iso_x = cart_x - cart_y + self.screen_width//2
-        iso_y = (cart_x + cart_y) / 2 + self.screen_height//2
-        return iso_x, iso_y
+    def camera(self, world_x, world_y):
+        screen_x = (world_x + world_y) // math.sqrt(2)
+        screen_y = (world_y - world_x) // math.sqrt(2)
+        return screen_x, screen_y
 
-    def draw_tile(self, pos_x, pos_y):
-        pointlist = [(pos_x - self.tile_size//2, pos_y - self.tile_size//2),
-                     (pos_x + self.tile_size//2, pos_y - self.tile_size//2),
-                     (pos_x + self.tile_size//2, pos_y + self.tile_size//2),
-                     (pos_x - self.tile_size//2, pos_y + self.tile_size//2),
-                     (pos_x - self.tile_size // 2, pos_y - self.tile_size//2)]
-        pygame.draw.polygon(self.screen, colors.FOREGROUND, pointlist, 1)
+    def draw_tile(self, world_x, world_y):
+        local_offsets = [(0, 0),
+                     (self.tile_size, 0),
+                     (self.tile_size, self.tile_size),
+                     (0, self.tile_size),
+                     (0, 0)]
+        iso_pointlist =[]
+
+        for local_x, local_y in local_offsets:
+            iso_pointlist.append(self.camera(local_x + world_x, local_y + world_y))
+        pygame.draw.polygon(self.screen, colors.FOREGROUND, iso_pointlist, 0) # fill
+        pygame.draw.polygon(self.screen, colors.BLACK, iso_pointlist, 1) #border
+        # pygame.draw.rect(self.screen, colors.BLUE, (self.camera(pos_x, pos_y), (4,4))) #origin
 
 
 if __name__ == "__main__":
