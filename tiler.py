eaf55a62d54e097ff9316ac70d80faf5adaee28c
smallship <user@example.com>
2018-12-21 19:14:23 +0000

    Improved: shader as function

--- tiler.py
+++ tiler.py
@@ -10,6 +10,7 @@ class Terrain:
     def __init__(self, map_size_x, map_size_y):
         self.map_size_x = map_size_x
         self.map_size_y = map_size_y
+        self.map_depth = map_size_x + map_size_y + 2
         self.map_grid = [[0 for _x in range(self.map_size_x)] for _y in range(self.map_size_y)]
 
     def perturb(self):
@@ -42,10 +43,18 @@ class Terrain:
         return random.randint(1, self.map_size_x - 2), random.randint(1, self.map_size_y - 2)
 
 
+def depth_shade(base_color, depth):
+    r, g, b = base_color
+    r = int(r + (255 - r) * depth * 0.4)
+    g = int(g + (255 - g) * depth * 0.4)
+    b = int(b + (255 - b) * depth * 0.45)
+    return r, g, b
+
+
 class Tiler:
     def __init__(self):
         self.terrain = Terrain(16, 16)
-        self.tile_size = 32 # 60
+        self.tile_size = 32  # 60
         self.terrain_height_scale = 0.5
         self.world_size = (self.terrain.map_size_x, self.terrain.map_size_y) * self.tile_size
         self.perturbs_per_update = 1
@@ -120,25 +129,18 @@ class Tiler:
                          (self.tile_size, self.tile_size),
                          (0, self.tile_size),
                          (0, 0)]
-        iso_pointlist = []
+        screen_pointlist = []
 
         for local_x, local_y in local_offsets:
             world_x, world_y = index_x * self.tile_size, index_y * self.tile_size
             world_height = -self.terrain.map_grid[index_x][index_y] * self.tile_size / 2
-            iso_pointlist.append(self.camera(local_x + world_x, local_y + world_y, world_height))
+            screen_pointlist.append(self.camera(local_x + world_x, local_y + world_y, world_height))
 
-        r, g, b = colors.GREEN
-        total_grid_depth = self.terrain.map_size_x + self.terrain.map_size_y - 2
-
-        current_grid_depth = total_grid_depth - (index_y + index_x)
-        r = int(r + (255 - r) * current_grid_depth / total_grid_depth * 0.7)
-        g = int(g + (255 - g) * current_grid_depth / total_grid_depth * 0.7)
-        b = int(b + (255 - b) * current_grid_depth / total_grid_depth * 0.7)
-        color_shade = (r, g, b)
-        # print(color_shade)
-
-        pygame.draw.polygon(self.screen, color_shade, iso_pointlist, 0)  # fill
-        pygame.draw.polygon(self.screen, colors.BACKGROUND, iso_pointlist, 1)  # border
+        current_map_depth = self.terrain.map_depth - (index_y + index_x)
+        # print((self.depth_shade(colors.GREEN, current_map_depth/self.terrain.map_depth)))
+        pygame.draw.polygon(self.screen, depth_shade(colors.GRASS, current_map_depth / self.terrain.map_depth),
+                            screen_pointlist)  # fill
+        pygame.draw.polygon(self.screen, colors.BACKGROUND, screen_pointlist, 1)  # border
         # pygame.draw.line(self.screen, colors.BLACK, iso_pointlist[1], iso_pointlist[2], 1)  # border
         # pygame.draw.line(self.screen, colors.BLACK, iso_pointlist[2], iso_pointlist[3], 1)  # border
         # pygame.draw.rect(self.screen, colors.BLUE, (self.camera(world_x, world_y), (4,4)))  # origin marker
